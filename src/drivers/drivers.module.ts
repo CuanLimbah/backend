a697a917ca9e257d6636e9431922c223f1401fb6
RenzIP <zenkun.enterkill13@gmail.com>
2026-04-29 19:24:02 +0700

    push

--- src/drivers/drivers.module.ts
+++ src/drivers/drivers.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PickupRoutesModule } from '../pickup-routes/pickup-routes.module';
 import { DriversController } from './drivers.controller';
 import { DriversService } from './drivers.service';
 
 @Module({
-  imports: [PickupRoutesModule],
+  imports: [AuthModule, PickupRoutesModule],
   controllers: [DriversController],
   providers: [DriversService],
   exports: [DriversService],
