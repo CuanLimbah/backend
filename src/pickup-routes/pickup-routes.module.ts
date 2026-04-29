4083d20995f1863fdbf5aaa64036e048936af6b9
RenzIP <zenkun.enterkill13@gmail.com>
2026-04-29 19:24:02 +0700

    push

--- src/pickup-routes/pickup-routes.module.ts
+++ src/pickup-routes/pickup-routes.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { PickupRoutesController } from './pickup-routes.controller';
 import { PickupRoutesService } from './pickup-routes.service';
 
 @Module({
+  imports: [AuthModule],
   controllers: [PickupRoutesController],
   providers: [PickupRoutesService],
   exports: [PickupRoutesService],
